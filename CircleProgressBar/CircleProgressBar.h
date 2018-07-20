0269a59cc00692774b2bc15bf99c8d3825d4bef7
yang-xu <yang.asahi@gmail.com>
2018-07-20 18:53:52 +0900

    first commit

---
+++ CircleProgressBar/CircleProgressBar.h
@@ -0,0 +1,19 @@
+//
+//  CircleProgressBar.h
+//  CircleProgressBar
+//
+//  Created by YANG XU on 2018/07/19.
+//  Copyright © 2018年 YANG XU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircleProgressBar.
+FOUNDATION_EXPORT double CircleProgressBarVersionNumber;
+
+//! Project version string for CircleProgressBar.
+FOUNDATION_EXPORT const unsigned char CircleProgressBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircleProgressBar/PublicHeader.h>
+
+
