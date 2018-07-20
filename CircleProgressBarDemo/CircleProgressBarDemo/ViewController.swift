3253235a557747315b1c912a995c04b4f8eea714
yang-xu <yang.asahi@gmail.com>
2018-07-20 19:28:35 +0900

    add the demo project

---
+++ CircleProgressBarDemo/CircleProgressBarDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CircleProgressBarDemo
+//
+//  Created by YANG XU on 2018/07/20.
+//  Copyright © 2018年 YANG XU. All rights reserved.
+//
+
+import UIKit
+import CircleProgressBar
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
