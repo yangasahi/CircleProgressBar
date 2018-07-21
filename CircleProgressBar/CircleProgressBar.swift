00f82ea87c52fcc9a68a1340d04a9a5e7bf6f20b
yang-xu <yang.asahi@gmail.com>
2018-07-22 02:44:54 +0900

    implement the base feature of show circle progress bar

---
+++ CircleProgressBar/CircleProgressBar.swift
@@ -0,0 +1,91 @@
+//
+//  CircleProgressBar.swift
+//  CircleProgressBar
+//
+//  Created by YANG XU on 2018/07/21.
+//  Copyright © 2018年 YANG XU. All rights reserved.
+//
+
+import UIKit
+
+public class CircleProgressBar: UIView {
+    
+    public var animationTimeDuring: CGFloat = 1.0
+    public var startAngle: CGFloat = 270
+    public var isClockwise = true
+    
+    private var circleProgressBarItems = [CircleProgressBarItem]()
+    private var isAnimating = false
+    
+    private var startValue: CGFloat = 0
+    private var indexOfArray = 0
+    
+    public func show() {
+        guard !isAnimating else {
+            return
+        }
+        isAnimating = true
+        self.layer.sublayers?.forEach {
+            $0.removeFromSuperlayer()
+        }
+        configCircleProgressBar()
+        
+    }
+    
+    public func add(_ circleProgressBarItem: CircleProgressBarItem) {
+        circleProgressBarItems.append(circleProgressBarItem)
+    }
+    
+    private func configCircleProgressBar() {
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.frame = CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height)
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineWidth = circleProgressBarItems[indexOfArray].strokeWidth
+        shapeLayer.strokeColor = circleProgressBarItems[indexOfArray].strokeColor.cgColor
+        
+        let center = CGPoint(x: shapeLayer.frame.width / 2, y: shapeLayer.frame.height / 2)
+        let radius = min(center.x, center.y)
+        
+        let uiBezierPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle / 180.0 * CGFloat.pi, endAngle: (startAngle + 360) / 180.0 * CGFloat.pi, clockwise: isClockwise)
+        
+        let caBasicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        caBasicAnimation.duration = getAnimationTime(from: circleProgressBarItems[indexOfArray].value)
+        caBasicAnimation.fromValue = startValue
+        caBasicAnimation.toValue = circleProgressBarItems[indexOfArray].value + startValue
+        caBasicAnimation.timingFunction = CAMediaTimingFunction(name: .linear)
+        caBasicAnimation.delegate = self
+        shapeLayer.add(caBasicAnimation, forKey: "strokeEnd")
+        
+        shapeLayer.path = uiBezierPath.cgPath
+        shapeLayer.strokeStart = startValue
+        shapeLayer.strokeEnd = circleProgressBarItems[indexOfArray].value + startValue
+        startValue = shapeLayer.strokeEnd
+        
+        indexOfArray += 1
+        
+        self.layer.addSublayer(shapeLayer)
+    }
+    
+    private func getAnimationTime(from value: CGFloat) -> TimeInterval {
+        var totalValue: CGFloat = 0
+        for circleProgressBarItem in circleProgressBarItems {
+            totalValue += circleProgressBarItem.value
+        }
+        return TimeInterval(value * animationTimeDuring / totalValue)
+    }
+}
+
+extension CircleProgressBar: CAAnimationDelegate {
+    public func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        guard flag else {
+            return
+        }
+        guard indexOfArray < circleProgressBarItems.count else {
+            indexOfArray = 0
+            startValue = 0
+            isAnimating = false
+            return
+        }
+        configCircleProgressBar()
+    }
+}
