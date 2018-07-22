ddbba32dae4ecfdee145128c075ef1f3329f5921
asahi <yang.asahi@gmail.com>
2018-07-22 14:40:59 +0900

    update demo (#4)

--- CircleProgressBarDemo/CircleProgressBarDemo/ViewController.swift
+++ CircleProgressBarDemo/CircleProgressBarDemo/ViewController.swift
@@ -15,9 +15,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let circlePrigressBarItem1 = CircleProgressBarItem(40.0 / 60.0, strokeWidth: 15, strokeColor: UIColor.green)
-        let circlePrigressBarItem2 = CircleProgressBarItem(15.0 / 60.0, strokeWidth: 10, strokeColor: UIColor.black)
-        let circlePrigressBarItem3 = CircleProgressBarItem(5.0 / 60.0, strokeWidth: 5, strokeColor: UIColor.brown)
+        let circlePrigressBarItem1 = CircleProgressBarItem(40.0 / 60.0, strokeWidth: 20, strokeColor: UIColor.green)
+        let circlePrigressBarItem2 = CircleProgressBarItem(15.0 / 60.0, strokeWidth: 13, strokeColor: UIColor.black)
+        let circlePrigressBarItem3 = CircleProgressBarItem(5.0 / 60.0, strokeWidth: 13, strokeColor: UIColor.brown)
+        
+        circleProgressBar.startAngle = 270
+        circleProgressBar.animationTimeDuring = 0.8
         
         circleProgressBar.add(circlePrigressBarItem1)
         circleProgressBar.add(circlePrigressBarItem2)
